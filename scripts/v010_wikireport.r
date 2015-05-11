64e2e24e84ce9f2bc511471181163fc613ffe01f
fgardos <fgardos@gmail.com>
2015-05-11 11:22:00 +0200

    nu3evo report

--- scripts/v010_wikireport.r
+++ scripts/v010_wikireport.r
@@ -6,7 +6,7 @@ R.version.string #"R version 3.0.0 RC (2013-03-26 r62418)"
 library (Biobase); packageDescription ("Biobase", fields = "Version") #"2.22.0"
 #library (mdgsa); packageDescription ("mdgsa", fields = "Version") #"0.3.3"
 library (TiddlyWikiR); packageDescription ("TiddlyWikiR", fields = "Version") #"1.0.4"
-help (package = TiddlyWikiR)
+#help (package = TiddlyWikiR)
 
 source (".job.r"); .job
 
@@ -44,8 +44,74 @@ mistags <- list ()
 # my_files <- dir('/media/FGG/tiddly/gsa4mirna/supplementary_files/files')
 # my_plots <- dir('/home/paco/Desktop/plots')
 
-my_files <- dir("/home/paco/Desktop/papers/gsa4mirna/supplementary_files/files")
-my_plots <- dir("/home/paco/Desktop/papers/gsa4mirna/supplementary_files/plots")
+my_files <- dir(file.path (.job$dir$code, "supplementary_files", "files"))
+my_plots <- dir(file.path (.job$dir$code, "supplementary_files", "plots"))
+
+
+## 0. Table with full information for all tumors
+################################################################################
+
+tabla.cols <- c ("ID", "miRNA.Down.unpa", "miRNA.noDif.unpa", "miRNA.Up.unpa",
+                 "miRNA.Down.pair", "miRNA.noDif.pair", "miRNA.Up.pair",
+                 "go.Down.unpa", "go.noDif.unpa", "go.UP.unpa",
+                 "go.Down.pair", "go.noDif.pair", "go.UP.pair",
+                 "bp.Down.unpa",  "bp.noDif.unpa", "bp.UP.unpa",
+                 "cc.Down.unpa",  "cc.noDif.unpa", "cc.UP.unpa",
+                 "mf.Down.unpa",  "mf.noDif.unpa", "mf.UP.unpa",
+                 "bp.Down.pair",  "bp.noDif.pair", "bp.UP.pair",
+                 "cc.Down.pair",  "cc.noDif.pair", "cc.UP.pair",
+                 "mf.Down.pair",  "mf.noDif.pair", "mf.UP.pair") 
+
+tabla.nams <- c ("ID", "Down.unpaired",  "noDif.unpaired",  "Up.unpaired", 
+                 "Down.paired",  "noDif.paired", "Up.paired",
+                 "Derg.unpaired",  "noDif.unpaired",  "Inh.unpaired",
+                 "Derg.paired",  "noDif.paired", "Inh.paired",
+                 "bp.Down.unpa",  "bp.noDif.unpa", "bp.UP.unpa",
+                 "cc.Down.unpa",  "cc.noDif.unpa", "cc.UP.unpa",
+                 "mf.Down.unpa",  "mf.noDif.unpa", "mf.UP.unpa",
+                 "bp.Down.pair",  "bp.noDif.pair", "bp.UP.pair",
+                 "cc.Down.pair",  "cc.noDif.pair", "cc.UP.pair",
+                 "mf.Down.pair",  "mf.noDif.pair", "mf.UP.pair") 
+
+tabla <- datos[,tabla.cols]
+colnames (tabla) <- tabla.nams
+
+
+tabla[,"resPaired"]   <- paste0 ("res_edger_paired_",   rownames (tabla), ".xlsx")
+tabla[,"resUnpaired"] <- paste0 ("res_edger_unpaired_", rownames (tabla), ".xlsx")
+
+tabla[,"resPaired_gsa_cc"]   <- paste0 ("res_gsa_paired_",     rownames (tabla), "_cc.xlsx")
+tabla[,"resPaired_gsa_mf"]   <- paste0 ("res_gsa_paired_",     rownames (tabla), "_mf.xlsx")
+tabla[,"resPaired_gsa_bp"]   <- paste0 ("res_gsa_paired_",     rownames (tabla), "_bp.xlsx")
+tabla[,"resUnpaired_gsa_cc"] <- paste0 ("res_gsa_unpaired_",   rownames (tabla), "_cc.xlsx")
+tabla[,"resUnpaired_gsa_mf"] <- paste0 ("res_gsa_unpaired_",   rownames (tabla), "_mf.xlsx")
+tabla[,"resUnpaired_gsa_bp"] <- paste0 ("res_gsa_unpaired_",   rownames (tabla), "_bp.xlsx")
+
+##
+links <- tabla  
+links[] <- NA  ## vaciado   #the same structure to TABLA but full of NA
+links[,"resPaired"]   <- paste0 ("supplementary_files/files/", tabla[,"resPaired"])
+links[,"resUnpaired"] <- paste0 ("supplementary_files/files/", tabla[,"resUnpaired"])
+
+links[,"resPaired_gsa_cc"]   <- paste0 ("supplementary_files/files/", tabla[,"resPaired_gsa_cc"])
+links[,"resUnpaired_gsa_cc"] <- paste0 ("supplementary_files/files/", tabla[,"resUnpaired_gsa_cc"])
+links[,"resPaired_gsa_mf"]   <- paste0 ("supplementary_files/files/", tabla[,"resPaired_gsa_mf"])
+links[,"resUnpaired_gsa_mf"] <- paste0 ("supplementary_files/files/", tabla[,"resUnpaired_gsa_mf"])
+links[,"resPaired_gsa_bp"]   <- paste0 ("supplementary_files/files/", tabla[,"resPaired_gsa_bp"])
+links[,"resUnpaired_gsa_bp"] <- paste0 ("supplementary_files/files/", tabla[,"resUnpaired_gsa_bp"])
+
+
+
+links <- as.matrix (links)
+#After generating the table, I have to wikify it. Good clue: in the template I have to indicate some tag to change this tag by the new table.
+#For this case, the tag is: @@~TABLA_all@@
+mistags[["@@~TABLA_all@@"]] <-  twTable (dat = tabla, ref = links, sortable = TRUE, align = c(rep("c",39)))
+writeTags (mistags, file = outfile)
+
+
+#save this general table:
+tabla0 <- tabla
+links0 <- links
 
 
 
@@ -57,9 +123,19 @@ tabla.nams <- c ("ID", "total",  "cases",  "controls",  "paired",  "description"
 tabla <- datos[,tabla.cols]
 colnames (tabla) <- tabla.nams
 
+#adding links to TCGA:
+tcga_links <- read.csv(file.path (.job$dir$proces, "tcga_links.csv"), sep = "\t", row.names = 1)
+tcga_links <- tcga_links[rownames(tabla),]
+tcga_links[,"mlinks"] <- paste0("[[", tcga_links[,"description"], "|", tcga_links[,"tcga_links"], "]]")
+head(tcga_links) 
+tabla1 <- cbind(tabla[, c("ID", "total", "cases", "controls", "paired")], tcga_links[,"mlinks"])
+colnames(tabla1) <-  c("ID", "total", "cases", "controls", "paired", "description")
+head(tabla1)
+
+
 #After generating the table, I have to wikify it. Good clue: in the template I have to indicate some tag to change this tag by the new table.
 #For this case, the tag is: @@~TABLA1_DATASETS@@
-mistags[["@@~TABLA1_DATASETS@@"]] <-  twTable (dat = tabla, sortable = TRUE, align = c("r", rep("c",4),"l"))
+mistags[["@@~TABLA1_DATASETS@@"]] <-  twTable (dat = tabla1, sortable = TRUE, align = c("r", rep("c",4),"l"))
 writeTags (mistags, file = outfile)
 
 
@@ -181,8 +257,8 @@ mistags[["@@~PLOTS_GENE_LEVEL@@"]] <- twTable (dat = plots, sortable = TRUE)
 ## TABLE 4
 ######################
 
-tabla.cols <- c ("ID", "targetsGOs.Down.unpa", "targetsGOs.Intersect.unpa", "targetsGOs.Up.unpa", "targetsGOs.Down.pair",
-                 "targetsGOs.Intersect.pair", "targetsGOs.Up.pair")
+tabla.cols <- c ("ID", "targetGOs.Down.unpa", "targetGOs.Intersect.unpa", "targetGOs.Up.unpa", "targetGOs.Down.pair",
+                 "targetGOs.Intersect.pair", "targetGOs.Up.pair")
 tabla.nams <- c ("ID", "Down.unpaired",  "Inter.unpaired",  "Up.unpaired",  "Down.paired",  "Inter.paired", "Up.paired")
 tabla <- datos[,tabla.cols]
 colnames (tabla) <- tabla.nams
@@ -193,28 +269,36 @@ writeTags (mistags, file = outfile)
 
 
 
+
+
+
+
 ## TABLE 5
 ######################
 
-tabla.cols <- c ("ID", "go.Down.unpa", "go.noDif.unpa", "go.UP.unpa", 
-                       "go.Down.pair", "go.noDif.pair", "go.UP.pair")
-tabla.nams <- c ("ID", "Derg.unpaired",  "noDif.unpaired",  "Inh.unpaired",  "Derg.paired",  "noDif.paired", "Inh.paired")
-tabla <- datos[,tabla.cols]
-colnames (tabla) <- tabla.nams
+tabla <- tabla0[, c ("ID", "Derg.unpaired",  "noDif.unpaired",  "Inh.unpaired", 
+                           "Derg.paired", "noDif.paired", "Inh.paired", 
+                     "bp.Down.unpa",  "bp.noDif.unpa", "bp.UP.unpa",
+                     "cc.Down.unpa",  "cc.noDif.unpa", "cc.UP.unpa",
+                     "mf.Down.unpa",  "mf.noDif.unpa", "mf.UP.unpa",
+                     "bp.Down.pair",  "bp.noDif.pair", "bp.UP.pair",
+                     "cc.Down.pair",  "cc.noDif.pair", "cc.UP.pair",
+                     "mf.Down.pair",  "mf.noDif.pair", "mf.UP.pair",
+                     "resPaired_gsa_cc", "resUnpaired_gsa_cc", "resPaired_gsa_mf",
+                     "resUnpaired_gsa_mf", "resPaired_gsa_bp",   "resUnpaired_gsa_bp")] 
 
-# tabla[,"resPaired"]   <- paste0 ("res_edger_paired_",   rownames (tabla), ".xlsx")
-# tabla[,"resUnpaired"] <- paste0 ("res_edger_unpaired_", rownames (tabla), ".xlsx")
-# ##
-# links <- tabla  
-# links[] <- NA  ## vaciado   #the same structure to TABLA but full of NA
-# links[,"resPaired"]   <- paste0 ("supplementary_files/files/", tabla[,"resPaired"])
-# links[,"resUnpaired"] <- paste0 ("supplementary_files/files/", tabla[,"resUnpaired"])
-# links <- as.matrix (links)
+links <- links0[, c ("ID", "Derg.unpaired",  "noDif.unpaired",  "Inh.unpaired", 
+                     "Derg.paired", "noDif.paired", "Inh.paired", 
+                     "bp.Down.unpa",  "bp.noDif.unpa", "bp.UP.unpa",
+                     "cc.Down.unpa",  "cc.noDif.unpa", "cc.UP.unpa",
+                     "mf.Down.unpa",  "mf.noDif.unpa", "mf.UP.unpa",
+                     "bp.Down.pair",  "bp.noDif.pair", "bp.UP.pair",
+                     "cc.Down.pair",  "cc.noDif.pair", "cc.UP.pair",
+                     "mf.Down.pair",  "mf.noDif.pair", "mf.UP.pair",
+                     "resPaired_gsa_cc", "resUnpaired_gsa_cc", "resPaired_gsa_mf",
+                     "resUnpaired_gsa_mf", "resPaired_gsa_bp",   "resUnpaired_gsa_bp")]
 
-#After generating the table, I have to wikify it. Good clue: in the template I have to indicate some tag to change this tag by the new table.
-
-# mistags[["@@~TABLA5_GOS@@"]] <-  twTable (dat = tabla, ref = links, sortable = TRUE, align = c(rep("c",7)), 
-mistags[["@@~TABLA5_GOS@@"]] <-  twTable (dat = tabla,  sortable = TRUE, align = c(rep("c",7)))
+mistags[["@@~TABLA5_GOS@@"]] <-  twTable (dat = tabla,  ref = links, sortable = TRUE, align = c(rep("c",31)))
 writeTags (mistags, file = outfile)
                                        
 
@@ -224,24 +308,32 @@ writeTags (mistags, file = outfile)
 ## Gene Set level
 ######################
 
-my_files2 <- my_files[grep("res_gsa_paired", my_files)]
-my_files2
+# my_files2 <- my_files[grep("res_gsa_paired", my_files)]
+# my_files2
+# links <- rep('NA', length(my_files2))
+# data <- cbind(my_files2, links)
+# class(data)
+# data[,"links"] <- paste ("[[",file.path (paste(my_files2,"supplementary_files/files",sep='|'), my_files2),"]]",sep="")
+# colnames(data) <- c("files", "links")
+# mistags[["@@~TABLA_GENE_SET_LEVEL_GO_PAIRED@@"]] <- twTable (dat = data, sortable = TRUE)
+# 
+# 
+# my_files2 <- my_files[grep("res_gsa_unpaired", my_files)]
+# my_files2
+# links <- rep('NA', length(my_files2))
+# data <- cbind(my_files2, links)
+# class(data)
+# data[,"links"] <- paste ("[[",file.path (paste(my_files2,"supplementary_files/files",sep='|'), my_files2),"]]",sep="")
+# colnames(data) <- c("files", "links")
+# mistags[["@@~TABLA_GENE_SET_LEVEL_GO_UNPAIRED@@"]] <- twTable (dat = data, sortable = TRUE)
+
+my_files2 <- c("common_enrichment_paired.xlsx", "common_enrichment_unpaired.xlsx" )
 links <- rep('NA', length(my_files2))
 data <- cbind(my_files2, links)
 class(data)
 data[,"links"] <- paste ("[[",file.path (paste(my_files2,"supplementary_files/files",sep='|'), my_files2),"]]",sep="")
 colnames(data) <- c("files", "links")
-mistags[["@@~TABLA_GENE_SET_LEVEL_GO_PAIRED@@"]] <- twTable (dat = data, sortable = TRUE)
-
-
-my_files2 <- my_files[grep("res_gsa_unpaired", my_files)]
-my_files2
-links <- rep('NA', length(my_files2))
-data <- cbind(my_files2, links)
-class(data)
-data[,"links"] <- paste ("[[",file.path (paste(my_files2,"supplementary_files/files",sep='|'), my_files2),"]]",sep="")
-colnames(data) <- c("files", "links")
-mistags[["@@~TABLA_GENE_SET_LEVEL_GO_UNPAIRED@@"]] <- twTable (dat = data, sortable = TRUE)
+mistags[["@@~TABLE_GENE_SET_LEVEL_ALL_STUDIES@@"]] <- twTable (dat = data, sortable = TRUE)
 
 
 my_plots2 <- my_plots[grep("paired_size_effect", my_plots)]
@@ -261,17 +353,11 @@ mistags[["@@~PLOTS_GENE_SET_LEVEL_SIZE_EFFECT_UNPAIRED@@"]] <- twTable (dat = pl
 
 
 
+
+
+
 ## Gene Set level
 ######################
-
-my_files2 <- c("allStudyInfo.xlsx", "common_enrichment_paired.xlsx", "common_enrichment_unpaired.xlsx" )
-links <- rep('NA', length(my_files2))
-data <- cbind(my_files2, links)
-class(data)
-data[,"links"] <- paste ("[[",file.path (paste(my_files2,"supplementary_files/files",sep='|'), my_files2),"]]",sep="")
-colnames(data) <- c("files", "links")
-mistags[["@@~TABLE_GENE_SET_LEVEL_ALL_STUDIES@@"]] <- twTable (dat = data, sortable = TRUE)
-
 
 my_plots2 <- c("paired_rindex_boxplot.png", "unpaired_rindex_boxplot.png")
 links <- rep('NA', length(my_plots2))
@@ -325,6 +411,12 @@ canceres <- c("blca", "brca", "cesc", "coad", "esca","hnsc", "kick", "kirc", "ki
               "read", "skcm", "stad", "thca", "ucec")
   
 for (i in 1:length(canceres)) {
+  
+  tabla <- tabla0[canceres[i],]
+  links <- as.data.frame(links0)[canceres[i],]
+  mistags[[paste("@@~SUMMARY_", canceres[i], "@@", sep ="")]] <-  twTable (dat = tabla,  ref = links, sortable = TRUE,
+                                                                           align = c(rep("c",39)))
+  
   my_plots2 <- my_plots[grep(canceres[i], my_plots)]
   links <- rep('NA', length(my_plots2))
   plots <- cbind(my_plots2, links)
@@ -356,14 +448,14 @@ writeTags (mistags, file = outfile)
 
 
 
-#TO CHECK:
-
-
-tabla1<-twTable (dat = tabla, ref = links, sortable = TRUE, title = "TABLE2")
-mistiddlers <- list ()
-mistiddlers[["dos"]] <- newTiddler (title = "BLCA2", content = list ("!dfañsdfja", tabla1, "sdafsdf", tabla1))
-writeTiddlers (mistiddlers, outfile = outfile)
-
+# #TO CHECK:
+# 
+# 
+# tabla1<-twTable (dat = tabla, ref = links, sortable = TRUE, title = "TABLE2")
+# mistiddlers <- list ()
+# mistiddlers[["dos"]] <- newTiddler (title = "BLCA2", content = list ("!dfañsdfja", tabla1, "sdafsdf", tabla1))
+# writeTiddlers (mistiddlers, outfile = outfile)
+# 
 
 
 
