04e3533f343831d2b6162abcfd3773c0734a941d
dmontaner <david.montaner@gmail.com>
2015-12-21 16:56:01 +0000

    sup mat godard in latex

--- supplementary_files_godard/z_make.sh
+++
@@ -1,4 +0,0 @@
-
-pandoc -s -S -f markdown -t html -o godard_comparison_report.html godard_comparison_report.md
-
-pandoc -s -S -f markdown -t latex -o godard_comparison_report.pdf godard_comparison_report.md
